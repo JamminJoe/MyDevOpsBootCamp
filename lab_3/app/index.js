a15b898393fa11e0e56d4e8b3b81c0d4be58bffe
JamminJoe <39530150+JamminJoe@users.noreply.github.com>
2020-04-28 10:30:39 -0700

    Fix index.js

--- lab_3/app/index.js
+++ lab_3/app/index.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(express.static("public"));
 
 app.get("/test", (req, res) => {
-  res.status(200).send("Hello-0, World!");
+  res.status(200).send("Hello, World!");
 });
 
 module.exports = app.listen(port, () => {
